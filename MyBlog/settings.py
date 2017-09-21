b2d8d5d67a92a08cf771e3c8a664861a4ac566ea
wuming.wang <wuming.wang@tinno.com>
2017-09-21 09:58:15 +0800

    '数据库配置'

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')^8bsdeh+hm*inwxt+wgeaovc1x%vvry=g&t(rf$xdk6=ltjnf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -70,16 +70,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MyBlog.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',
+        'USER': 'root',
+        'PASSWORD': '291253',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
